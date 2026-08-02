2ac4b4f8a8158d1451219f1f9b6fe1117bea3f8c
tkdjw3295-blip <tkdjw3295@gmail.com>
2026-08-02 15:38:34 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,26 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "training-log-cache-v1";
+const urlsToCache = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
   clients.claim();
 });
